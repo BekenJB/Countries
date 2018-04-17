8da0216121fa51615989394870e1ad86ed48826c
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2018-04-17 19:52:08 -0400

    Add project files.

---
+++ Countries/Controllers/CountriesController.cs
@@ -0,0 +1,123 @@
+﻿using Countries.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Countries.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Countries")]
+    public class CountriesController : Controller
+    {
+        private readonly CountryContext _context;
+
+        public CountriesController(CountryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public IEnumerable<Country> GetCountries()
+        {
+            return _context.Countries;
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCountry([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var country = await _context.Countries.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        // PUT: api/Countries/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry([FromRoute] long id, [FromBody] Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        [HttpPost]
+        public async Task<IActionResult> PostCountry([FromBody] Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var country = await _context.Countries.SingleOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return Ok(country);
+        }
+
+        private bool CountryExists(long id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Countries/Models/CountryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Countries.Models
+{
+    public class CountryContext:DbContext
+    {
+        public CountryContext(DbContextOptions<CountryContext> options): base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
